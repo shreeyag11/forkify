fb5ade1d6a74b10dbcc955cc1cacb2b3ea6bd78f
Shreeya Gupta <shreeyag11@gmail.com>
2020-05-27 18:36:53 -0400

    able to fetch data from api

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,13 @@
 // Global app controller
-import x from './test';
-const y = 32;
-console.log(`${x} + ${y}`);
+import axios from 'axios';
+
+async function getResult(query) {
+    const proxy = 'https://cors-anywhere.herokuapp.com/';
+    try {
+        const res = await axios(`${proxy}https://forkify-api.herokuapp.com/api/search?&q=${query}`);
+        console.log(res.data.recipes);
+    } catch (error) {
+        alert(error);
+    }
+}
+getResult('pizza');
